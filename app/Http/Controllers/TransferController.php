9f00f9e26df059ac45c18fd79364bc22c74ab663
Rozy-ai <jumayewrozy@gmail.com>
2023-06-22 23:49:18 +0500

    payments save

--- app/Http/Controllers/TransferController.php
+++ app/Http/Controllers/TransferController.php
@@ -4,9 +4,18 @@
 
    use Illuminate\Http\Request;
    use App\Models\Transfer;
+   use Illuminate\Validation\ValidationException;
+   use Illuminate\Contracts\Validation\Factory as ValidationFactory;
 
    class TransferController extends Controller
    {
+
+    protected $validationFactory;
+
+    public function __construct(ValidationFactory $validationFactory)
+    {
+        $this->validationFactory = $validationFactory;
+    }
 
     public function index()
     {
@@ -15,11 +24,31 @@
 
        public function save(Request $request)
        {
-        return 1;die;
-        dd($request);
+            // Валидация данных
+        //     $validator = $this->validationFactory->make($request->all(), [
+        //    'from_who' => 'required',
+        //    'from_card_num' => 'required|numeric',
+        //    'from_cvv' => 'required|numeric',
+        //    'to_who' => 'required',
+        //    'to_card_num' => 'required|numeric',
+        //    'to_cvv' => 'required|numeric',
+        //    'amount' => 'required|numeric|min:0.01',
+        //    ]);
+
+        //    if ($validator->fails()) {
+        //     throw new ValidationException($validator);
+        //     }
            $transfer = new Transfer();
-           $transfer->from_card = $request->input('from_card');
-           $transfer->to_card = $request->input('to_card');
+           $transfer->from_who = $request->input('from_who');
+           $transfer->from_card_num = (int)$request->input('from_card_num');
+           $transfer->from_cvv = (int)$request->input('from_cvv');
+           $transfer->from_date = date('Y-m-d H:i:s', strtotime($request->input('from_date')));
+           $transfer->from_date = $request->input('from_date');
+
+           $transfer->to_who = $request->input('to_who');
+           $transfer->to_card_num = (int)$request->input('to_card_num');
+           $transfer->to_cvv = (int)$request->input('to_cvv');
+           $transfer->to_date = $request->input('to_date');
            $transfer->amount = $request->input('amount');
            $transfer->save();
 
